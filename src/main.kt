a60f08c5c550bbf437f47a81c5efc3a214cb8de9
tanyamayiana <tanyamayiana@gmail.com>
2022-03-10 08:55:58 +0300

    first commit

---
+++ src/main.kt
@@ -0,0 +1,35 @@
+fun main(){
+    program()
+    var outcome = product(11,5)
+    println(outcome)
+    i= info("tanyasis",21)
+    println(i)
+    statment()
+
+}
+fun program(){
+    var name = "CodeHive"
+    println(name [4].toString()name[5],name[6],name[7],name[8])
+
+
+
+}
+
+fun println(toString: String, c: Char, c1: Char, c2: Char) {
+
+}
+
+fun product(Num1:Int Num2:Int):Int{
+    var division = (Num1/Num2)
+    return division
+
+}
+//fun info(name1:String, age:Int):String{
+    var name = "p"
+    var age = z
+    println($my "p" is Tanyasis i am "z" years old)
+}
+fun statment():Int{
+    var text = "I am a student in AkiraChiks"
+    return text.length
+}
